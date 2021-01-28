c99646d9f211ebc6b3a1a3523caaf17118afe9ab
Raushan <raushan.043.kumar@gmail.com>
2021-01-28 15:35:12 +0530

    completed except search api

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const keys="https://api.themoviedb.org/3/movie/popular?api_key=7b34dacdf4309d45bcfe6b218078bbc5";
 const container= document.querySelector('.container');
 const store= document.querySelector('.store')
+const cross= document.querySelector('.cross');
+const upper=document.querySelector('.upper');
+const detail= document.querySelector('.detail');
+const body = document.body;
+upper.style.top=0 +'px';
+// const details= document.querySelector('.details');
+cross.addEventListener('click',()=>{
+   detail.innerHTML='';
+    upper.classList.add('hidden');
+    body.style.overflow='auto'
+})
 // console.log(store);
 // console.log(container);
 const IMGPATH= "https://image.tmdb.org/t/p/w1280";
@@ -21,9 +32,9 @@ const Result= async()=>{
 
 const getresult= Result();
   getresult.then((e)=>{
- 
-    e.results.forEach(element => {
-        const path=(element.backdrop_path);
+//    console.log(e);
+    e.results.forEach(element1 => {
+        const path=(element1.poster_path);
         const realpath= IMGPATH+path;
         
         const inside =document.createElement('div');
@@ -31,16 +42,16 @@ const getresult= Result();
         const html=`
         <img  class="image" src="${realpath}">
         <div class="content">
-            <h1>${element.original_title}</h1>
+            <h1>${element1.original_title}</h1>
             <span >9</span>
         </div>`
         inside.innerHTML=html;
           store.insertAdjacentElement('beforeend',inside);
-         var control= true;
+        //  var control= true;
         const image= document.querySelectorAll('.image');
-        console.log("outer");
+        // console.log("outer");
         image.forEach(element => {
-                console.log(control);
+                // console.log(control);
               element.addEventListener('click',(e)=>{
             //   controls=true;
                 const X= e.offsetX;
@@ -51,7 +62,10 @@ const getresult= Result();
                 // console.log(inside.contains('span'));
                 const fre= document.querySelector('.ripple')
                 if(fre==null)
+                {
                 inside.appendChild(ripple);
+                
+                }
                 
                 ripple.style.left=X +"px";
                 ripple.style.top=Y +"px";
@@ -68,12 +82,44 @@ const getresult= Result();
               
             })
         // console.log("outer");
+        element.addEventListener('click',(e)=>{
+            // console.log(element1);
+            setTimeout(() => {
+                showdetails(element1);
+            }, 1000);
+           
+        })
         });
         // control =control+1;
-    });
+      
+    }
+      
+    );
     
   })
   
-
+function showdetails(imge_path)
+{
+    // console.log("inside the dive");
+    // console.log(imge_path);
+    const detailss= document.querySelector('.details');
+    if(detailss==null)
+    {
+    const details= document.createElement('div');
+    details.setAttribute('class','details');
+    detail.innerHTML='';
+    const html=`
+                     <img src=${IMGPATH+imge_path.poster_path}>
+                     <h1 class="overview">Overview</h1>
+      <h4>${imge_path.overview}</h4>`
+      details.innerHTML=html;
+      detail.insertAdjacentElement('beforeend',details);
+      upper.classList.remove('hidden');
+    //   console.log(document.body);
+//   body.style.top=0+'px';
+   upper.style.top= window.scrollY +'px';
+    body.style.overflow='hidden';
+    }
+}
 
   
